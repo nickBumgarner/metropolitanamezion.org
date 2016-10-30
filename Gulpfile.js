7a8043ec72387e35e708c744a89cc26bc2a7230e
Nicholas Bumgarner <nick@inclind.com>
2016-10-30 11:00:47 -0400

    Initializing repository. Project is running Neat, Bourbon and Gulp. Must run NPM Install and then gulp watch to server files. Initial commit includes almost all of the content on the page and a good portion of theming as well.

---
+++ Gulpfile.js
@@ -0,0 +1,38 @@
+"use strict";
+
+var gulp = require("gulp"),
+    browserSync = require("browser-sync"),
+    sass = require("gulp-sass"),
+    bourbon = require("node-bourbon").includePaths,
+    neat = require("node-neat").includePaths;
+
+// Compiles all gulp tasks
+gulp.task("default", ["sass"]);
+
+// Live reload anytime a file changes
+gulp.task("watch", ["browserSync", "sass"], function() {
+  gulp.watch("scss/**/*.scss", ["sass"]);
+  gulp.watch("*.html").on("change", browserSync.reload);
+});
+
+// Spin up a server
+gulp.task("browserSync", function() {
+  browserSync({
+    server: {
+      baseDir: ""
+    }
+  })
+});
+
+// Compile SASS files
+gulp.task("sass", function() {
+  gulp.src("scss/**/*.scss")
+      .pipe(sass({
+        includePaths: bourbon,
+        includePaths: neat
+      }))
+      .pipe(gulp.dest("css"))
+      .pipe(browserSync.reload({
+        stream: true
+      }))
+});
