24e99170ba3c44cc27434debd5049741ea9a34d4
Nicholas Bumgarner <nick@inclind.com>
2016-11-02 18:57:11 -0400

    Making a few adjustments to the theme including vertically centering the social media links and setting max width on triptych regions

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,6 @@
 $(document).ready(function () {
+
+  //Open modal windows
   $('a.modal-link').click(function (e) {
     e.preventDefault();
     var modalTarget = $(this).attr('id');
@@ -6,10 +8,13 @@ $(document).ready(function () {
     $('body').addClass('modal-open');
   });
 
+  //Close modal windows
   $('.close-button, .screen').click(function () {
     $('.modal-window').removeClass('open');
     $('body').removeClass('modal-open');
   });
+
+  //Close modal windows with escape key
   $(document).keyup(function(e) {
     if (e.keyCode == 27) { // escape key maps to keycode `27`
       $('.modal-window').removeClass('open');
